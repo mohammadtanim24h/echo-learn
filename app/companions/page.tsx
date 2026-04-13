aefa932047475d33a36456bf03cccdcef2e101fe
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2026-04-13 19:05:38 +0600

    feat: fetch companions from db and display

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,3 +1,32 @@
-export default function CompanionsLibrary() {
-  return <div>Companions Library</div>;
+import CompanionCard from "@/components/CompanionCard";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+import { SearchParams } from "@/types";
+
+export default async function CompanionsLibrary({
+    searchParams,
+}: SearchParams) {
+    const filters = await searchParams;
+    const subject = filters.subject ? filters.subject : "";
+    const topic = filters.topic ? filters.topic : "";
+
+    const companions = await getAllCompanions({ subject, topic });
+
+    return (
+        <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="flex gap-4">Filters</div>
+            </section>
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+        </main>
+    );
 }
