647b6535ae8be4e7e7729da56747670df2177cfa
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2026-04-13 17:08:57 +0600

    feat: add server action to retrieve companions

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { CreateCompanion } from "@/types";
+import { CreateCompanion, GetAllCompanions } from "@/types";
 import { auth } from "@clerk/nextjs/server";
 import { createClient } from "../supabase";
 
@@ -16,4 +16,30 @@ export const createCompanion = async (formData: CreateCompanion) => {
     if (error || !data)
         throw new Error(error?.message || "Failed to create companion");
     return data[0];
+};
+
+export const getAllCompanions = async ({
+    limit = 10,
+    page = 1,
+    subject,
+    topic,
+}: GetAllCompanions) => {
+    const supabase = createClient();
+    let query = supabase.from("companions").select();
+    if (subject && topic) {
+        query = query
+            .ilike("subject", `%${subject}%`)
+            .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+    } else if (subject) {
+        query = query.ilike("subject", `%${subject}%`);
+    } else if (topic) {
+        query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+    }
+
+    query = query.range((page - 1) * limit, page * limit - 1);
+
+    const { data: companions, error } = await query;
+    if (error) throw new Error(error.message || "Failed to fetch companions");
+
+    return companions;
 };
