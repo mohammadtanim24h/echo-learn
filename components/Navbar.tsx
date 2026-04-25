6d612678cdfa1dbbd00989795a0cadc2e29a7bd6
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2026-04-25 23:42:18 +0600

    refactor: update logo and favicon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,9 +9,9 @@ export default function Navbar() {
             <Link href={"/"}>
                 <div className="flex items-center gap-2.5 cursor-pointer">
                     <Image
-                        src="/images/logo.svg"
+                        src="/images/logo.png"
                         alt="logo"
-                        width={46}
+                        width={130}
                         height={44}
                     ></Image>
                 </div>
